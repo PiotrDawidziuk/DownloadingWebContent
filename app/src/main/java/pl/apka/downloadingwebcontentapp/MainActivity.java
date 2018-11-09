01a69608bac155932e8c85c2abcb30139f0bb18a
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-11-09 22:11:26 +0100

    task added

--- app/src/main/java/pl/apka/downloadingwebcontentapp/MainActivity.java
+++ app/src/main/java/pl/apka/downloadingwebcontentapp/MainActivity.java
@@ -18,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://zappycode.com/");
     }
 }
