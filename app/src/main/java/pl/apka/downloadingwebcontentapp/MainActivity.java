a8ac75fc4dd2c22217644cb4a4ae277963eb1752
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-11-09 22:08:00 +0100

    first commit

---
+++ app/src/main/java/pl/apka/downloadingwebcontentapp/MainActivity.java
@@ -0,0 +1,22 @@
+package pl.apka.downloadingwebcontentapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
